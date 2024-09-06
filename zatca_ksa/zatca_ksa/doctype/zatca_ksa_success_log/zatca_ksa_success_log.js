e518e9610fdb6a68dfc065b3018bec7fc0b7a1a5
basondowah <basondowah@gmail.com>
2024-09-06 21:39:38 +0300

    first commit

---
+++ zatca_ksa/zatca_ksa/doctype/zatca_ksa_success_log/zatca_ksa_success_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, KSA and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Zatca KSA Success Log", {
+// 	refresh(frm) {
+
+// 	},
+// });
